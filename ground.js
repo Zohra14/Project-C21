17772d80b58321a8cf3278be6b0998631aaa91ef
Zohra14 <104147704+Zohra14@users.noreply.github.com>
2022-07-12 17:28:20 +0400

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,13 @@
+class Ground{
+ constructor(x,y,w,h){
+     var options={
+         isstatic:true
+     }
+     this.x=x
+     this.y=y
+     this.w=w
+     this.h=h
+     this.body=Bodies.rectangle(x,y,w,h,options)
+     World.add(world,this.body)
+     }
+}
---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball
+var gd,lgd,rgd
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+ var ball_options={
+	 isStatic:false,
+	 restitution:0.4,
+	 density:1.2
+ }
+
+ ball = Bodies.circle(200,100,20,ball_options);
+World.add(world,ball);
+
+gd= new Ground(width/2,670,width,20)
+lgd= new Ground (1100,600,20,120)
+rgd= new Ground (1350,600,20,120)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  background(0);
+  ellipse(ball.position.x,ball.position.y,20)
+  gd.display()
+  lgd.display()
+  rgd.display()
+
+  drawSprites();
+ 
+}
+
+
+
